b07be11ee8e62a5602be45b9daed7f719badb9e1
ACHYUTANANDA-SAHOO <113246116+ACHYUTANANDA-SAHOO@users.noreply.github.com>
2022-09-30 23:52:57 +0530

    Update lru.c

--- DAY 4 LAB/Exp17_LRU/lru.c
+++ DAY 4 LAB/Exp17_LRU/lru.c
@@ -1,75 +1,106 @@
 #include<stdio.h>
- 
-    int findLRU(int time[], int n){
-    int i, minimum = time[0], pos = 0;
-    
-    for(i = 1; i < n; ++i){
-        if(time[i] < minimum){
-            minimum = time[i];
-            pos = i;
-        }
-    }
-    return pos;
-    }
-    
-    int main()
-    {
-        int no_of_frames, no_of_pages, frames[10], pages[30], counter = 0, time[10], flag1, flag2, i, j, pos, faults = 0;
-        
-        printf("Enter number of frames: ");
-        scanf("%d", &no_of_frames);
-        printf("Enter number of pages: ");
-        scanf("%d", &no_of_pages);
-        printf("Enter reference string: ");
 
-        for(i = 0; i < no_of_pages; ++i){
-            scanf("%d", &pages[i]);
-        }
-
-        for(i = 0; i < no_of_frames; ++i){
-            frames[i] = -1;
-        }
-        
-        for(i = 0; i < no_of_pages; ++i){
-            flag1 = flag2 = 0;
-        
-        for(j = 0; j < no_of_frames; ++j){
-                if(frames[j] == pages[i]){
-                counter++;
-                time[j] = counter;
-                flag1 = flag2 = 1;
-                break;
-            }
-        }
-        
-        if(flag1 == 0){
-        for(j = 0; j < no_of_frames; ++j){
-            if(frames[j] == -1){
-                counter++;
-                faults++;
-                frames[j] = pages[i];
-                time[j] = counter;
-                flag2 = 1;
-                break;
-            }
-            }
-        }
-        
-        if(flag2 == 0){
-            pos = findLRU(time, no_of_frames);
-            counter++;
-            faults++;
-            frames[pos] = pages[i];
-            time[pos] = counter;
-        }
-        
-        printf("\n");
-        
-        for(j = 0; j < no_of_frames; ++j){
-            printf("%d\t", frames[j]);
-        }
+int check_presence(int goal, int arr[], int size){
+    for(int i = 0;i<size;i++){
+        if(goal == arr[i])
+            return 1;
     }
-    printf("\n\nTotal Page Faults = %d", faults);
-    
     return 0;
+}
+
+int print_page_frame(int arr[], int size){
+    for(int i = 0; i<size;i++){
+        printf("%d \t", arr[i]);
+    }
+}
+
+int find_LRU(int arr[], int s){
+    int minimum = arr[0];
+    for(int i = 0;i<s;i++){
+        if(arr[i] < minimum)
+            minimum = arr[i];
+    }
+    return minimum;
+}
+
+int main(){
+    int f;
+    printf("Enter Frame Size : ");
+    scanf("%d", &f);
+    int frame[f];
+
+    int r;
+    printf("Enter Size of Reference String : ");
+    scanf("%d", &r);
+
+    int ref_str[r];
+
+    printf("Enter Reference String : \n");
+    for(int i = 0;i<r;i++){
+        scanf("%d", &ref_str[i]);
+    }
+
+    int time[f];
+    for(int i = 0;i<f;i++){
+        time[i] = 0;
+    }
+    for(int i = 0; i<f;i++){
+        frame[i] = -1;
+    }
+
+    int page_ptr = -1;
+    int count = 0;
+    int hits = 0;
+    int misses = 0;
+    int t = 0;
+    int negatives = f;
+
+    print_page_frame(frame,f);
+    printf("\n");
+
+    while(count < r){
+        if(negatives != 0){
+            page_ptr = page_ptr + 1;
+
+            if (check_presence(ref_str[count],frame,f) == 1){
+                print_page_frame(frame,f);
+                printf(" \t ----- HIT \n");
+                page_ptr-=1;
+                time[page_ptr-1]= count;
+                hits+=1;
+            }
+            else if(check_presence(ref_str[count],frame,f) == 0){
+                frame[page_ptr] = ref_str[count];
+                time[page_ptr] = count;
+                print_page_frame(frame,f);
+                printf(" \t ----- MISS \n");
+                negatives-=1;
+                misses+=1;
+            }
+            
+        }
+        else{
+            page_ptr = find_LRU(time,f);
+
+            if (check_presence(ref_str[count],frame,f) == 1){
+                print_page_frame(frame,f);
+                printf(" \t ----- HIT \n");
+                time[page_ptr-1]= count;
+                hits+=1;
+            }
+            else if(check_presence(ref_str[count],frame,f) == 0){
+                frame[page_ptr] = ref_str[count];
+                time[page_ptr] = count;
+                print_page_frame(frame,f);
+                printf(" \t ----- MISS \n");
+                negatives-=1;
+                misses+=1;
+            }
+        }
+        print_page_frame(time,f);
+        printf("\n");
+        count = count + 1;
+    }
+    printf("\nNo of Hits = %d \n", hits);
+    printf("No. Of Misses = %d \n", misses);
 }
